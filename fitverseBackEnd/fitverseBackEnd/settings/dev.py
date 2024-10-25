9e4d0eb528b5faf2fd671bfec36e3de7e09d2f06
Pilli-Venkat <venkatasai2920@gmail.com>
2024-10-25 18:56:04 +0530

    Added static ip address

--- fitverseBackEnd/fitverseBackEnd/settings/dev.py
+++ fitverseBackEnd/fitverseBackEnd/settings/dev.py
@@ -5,6 +5,6 @@ from .base import *
 # Development-specific settings
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','13.232.74.166']
 
 # You can add other development-specific settings here
